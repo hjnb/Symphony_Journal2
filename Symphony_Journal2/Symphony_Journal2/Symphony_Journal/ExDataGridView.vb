66b6f8f76a5b9532faed55817f395324486d01d2
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-04-11 09:41:51 +0900

    ユニット日誌：ctrl + x で選択セル内容切り取り処理追加

--- Symphony_Journal2/Symphony_Journal2/Symphony_Journal/ExDataGridView.vb
+++ Symphony_Journal2/Symphony_Journal2/Symphony_Journal/ExDataGridView.vb
@@ -63,4 +63,16 @@
         Return newData
     End Function
 
+    Private Sub ExDataGridView_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        'ctrl + x で選択セル内容切り取り
+        If e.Control AndAlso e.KeyCode = Keys.X Then
+            '選択セルの内容コピー
+            Clipboard.SetDataObject(GetClipboardContent())
+
+            '選択セルの内容削除
+            For Each cell As DataGridViewCell In SelectedCells
+                cell.Value = ""
+            Next
+        End If
+    End Sub
 End Class
